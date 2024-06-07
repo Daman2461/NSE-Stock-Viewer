95784357bdc4c210581fa0e3ddd07863007cf0db
Daman2461 <142651012+Daman2461@users.noreply.github.com>
2024-06-07 16:08:50 +0530

    Update stock_app.py

--- stock_app.py
+++ stock_app.py
@@ -71,7 +71,7 @@ fig.update_layout(
 
 # Display the graph and the dataframe in the same row
 
-col1,col2=st.beta_columns((1,3))
+col1,col2=st.columns((1,3))
 with col1:
     st.dataframe(df[company],use_container_width=True)
 with col2:
