8fcd745109a8ce8643c794a2c69c33e98b9bfbdf
Daman2461 <142651012+Daman2461@users.noreply.github.com>
2024-06-07 18:48:17 +0530

    Update stock_app.py

--- stock_app.py
+++ stock_app.py
@@ -34,12 +34,12 @@ with st.sidebar:
    key='company'
     )
 
-    period = st.selectbox(
-   "Period",
-   ('1d', '5d', '1mo', '3mo', '6mo', '1y', '2y', '5y', '10y', 'ytd', 'max'),
-   index=2,
-   key='period'
-    )
+   period = st.radio(
+    "Period",
+    options=['1d', '5d', '1mo', '3mo', '6mo', '1y', '2y', '5y', '10y', 'ytd', 'max'],
+    index=2,
+    key='period'
+)
     interval = st.selectbox(
    "Interval",
    ('1m', '2m','5m', '15m', '30m', '60m', '90m', '1h', '1d', '5d', '1wk', '1mo', '3mo'),
